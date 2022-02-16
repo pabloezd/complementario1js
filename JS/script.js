d3a483243f6c23652cc27e14536b89e775265a86
Pablo Duarte <pablo@Air-de-Pablo.fibertel.com.ar>
2022-02-15 21:19:23 -0300

    ooo

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 // Preguntamos el nombre y saludamos al usuario
 let saludarUsuario = prompt("Ingrese su nombre");
-alert("Lo estabamos esperando " + saludarUsuario);
+alert("Te estabamos esperando " + saludarUsuario);
 
 //Pedimos que el usuario ingrese un numero menor a 20 para empezar a contar
 let i = prompt("Ingrese un numero menor a 20");
