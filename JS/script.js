482c491da98dd7382400e3d9277348069ae1e26f
Pablo Duarte <pablo@Air-de-Pablo.fibertel.com.ar>
2022-02-15 21:08:24 -0300

    nuevoArchivo

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+
+let saludarUsuario = prompt("Ingrese su nombre")
+alert("Lo estabamos esperando " + saludarUsuario)
+
+let i = prompt("Ingrese un numero menor a 20");
+
+for (i ; i < 20; i++) {
+    alert(i);
+}
+
